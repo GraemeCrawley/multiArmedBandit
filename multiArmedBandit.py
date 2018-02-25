fb312689b7f9ca2c9e77eae93dffa64f726452e7
Graeme <gacrawley@gmail.com>
2018-02-25 15:43:06 -0500

    seems to work fine. I'll output some graphs to see the results

--- multiArmedBandit.py
+++ multiArmedBandit.py
@@ -4,7 +4,7 @@ armLength = 5
 
 
 #Epsilon value
-epsilon = 0.01
+epsilon = 0.1
 
 #Values of u to be randomized
 u = [0,0,0,0,0]
@@ -15,24 +15,25 @@ uHat = [0,0,0,0,0]
 
 
 #all turns
-for t in range(10):
+for t in range(1000):
+	print("\n\n\n\nRound " + str(t))
 	maxArmValue = 0
 	maxArmI = 0
 	chosenArm = 0
 	chosenArmI = 0
 	#update the expected values of each arm, randomly
-	print("Randomizing arms: \n")
+	print("Randomizing arms:")
 	for i in range(armLength):
 		u[i] = random.uniform(0,100)
-		print("Arm " + str(i) + " random value: " + str(u[i]) + "\n")
+		print("Arm " + str(i) + " random value: " + str(u[i]))
 
 	#Toss a coin
 	#random value to choose which arm to select:
 	randomValue = random.uniform(0,1)
-	print("Coin toss: " + str(randomValue) + "\n")
+	print("\nCoin toss: " + str(randomValue))
 
 	#find the maximum empirical mean
-	print("Finding the maximum average value: \n")
+	print("Finding the maximum average value:")
 	for i in range(armLength):
 		if uHat[i] > maxArmValue:
 			maxArmValue = uHat[i]
@@ -41,20 +42,20 @@ for t in range(10):
 			continue
 	print("Current max average arm is arm " + str(maxArmI) + " with value " + str(maxArmValue) + "\n")
 
-	print("Choosing based on coin toss: \n")
+	print("Choosing based on coin toss")
 	#Use epsilon to choose either a random arm or the currently best arm
 	if(randomValue > epsilon):
-		print("Chosen current best arm: \n")
+		print("Chosen current best arm")
 		#Choose the currently best arm
 		chosenArm = u[maxArmI]
 		chosenArmI = maxArmI
 	else:
-		print("Chosen random arm: \n")
+		print("Chosen random arm")
 		#choose a random arm
 		randomArmChoice = random.randint(0,armLength-1)
 		chosenArm = u[randomArmChoice]
 		chosenArmI = randomArmChoice
-	print("Chosen arm is arm " + str(chosenArmI) + "\n")
+	print("Chosen arm is arm " + str(chosenArmI))
 
 	uHat[chosenArmI] = (uHat[chosenArmI]*(t) + chosenArm)/(t+1)
 
