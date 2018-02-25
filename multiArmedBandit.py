af7172616d46d1fee2f9aa2f24e5d37555dbb870
Graeme <gacrawley@gmail.com>
2018-02-25 15:11:02 -0500

    added branching based on a coin flip. This has been helpful: http://slivkins.com/work/MAB-book.pdf

--- multiArmedBandit.py
+++ multiArmedBandit.py
@@ -1,27 +1,45 @@
 import random
 dist = [0,0,0,0,0]
 
-var = [0.1^2,0.01^2,0.05^2,0.03^2,0.08^2]
 
 maxExp = 0
 
-epsilon = 0.1
+#Epsilon value
+epsilon = 0.01
+
+#5 expected values for 5 arms
+uHat = [0,0,0,0,0]
+
+#5 Variances for 5 arms
+var = [0.1^2,0.01^2,0.05^2,0.03^2,0.08^2]
+
+#Initializing probabilities
+p = [0,0,0,0,0,0,0,0,0,0]
+
 
 #all turns
 for t in range(100):
-	uHat = [1,1,1,1,1]
-	p = [0,0,0,0,0,0,0,0,0,0]
+	#random value to choose which arm to select:
+	randomValue = uniform.random(0,1)
 
-	for i in exp:
-		exp[i] = random.uniform(0,1)
+	#find the maximum expected value
+	for i in uHat:
+		if i > maxExp:
+			maxExp = uHat[i]
+		else:
+			continue
+
+
+	if(randomValue > epsilon):
+		#select the arm with the highest empirical probability
+	else:
+		#select a random arm
+
 
 
 	#update the expected value every turn
+	for i in uHat:
+		#some way to update uHat
 
-	comment: find the maximum expected value
-	for i in exp:
-		if i > maxExp:
-			maxExp = i
-		else:
-			continue
+
 
