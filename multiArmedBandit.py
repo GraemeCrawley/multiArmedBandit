1e6944fabbfd8b52f0dd0f616f99b0b1ea7a7f59
Graeme <gacrawley@gmail.com>
2018-02-25 15:36:33 -0500

    added outputs

--- multiArmedBandit.py
+++ multiArmedBandit.py
@@ -6,40 +6,56 @@ armLength = 5
 #Epsilon value
 epsilon = 0.01
 
-#5 expected values for 5 arms
+#Values of u to be randomized
+u = [0,0,0,0,0]
+
+#Average values of u
 uHat = [0,0,0,0,0]
 
 
+
 #all turns
-for t in range(100):
+for t in range(10):
 	maxArmValue = 0
 	maxArmI = 0
+	chosenArm = 0
+	chosenArmI = 0
 	#update the expected values of each arm, randomly
-	for i in uHat:
-		uHat[i] = random.uniform(0,100)
+	print("Randomizing arms: \n")
+	for i in range(armLength):
+		u[i] = random.uniform(0,100)
+		print("Arm " + str(i) + " random value: " + str(u[i]) + "\n")
 
 	#Toss a coin
 	#random value to choose which arm to select:
 	randomValue = random.uniform(0,1)
+	print("Coin toss: " + str(randomValue) + "\n")
 
 	#find the maximum empirical mean
-	for i in uHat:
+	print("Finding the maximum average value: \n")
+	for i in range(armLength):
 		if uHat[i] > maxArmValue:
-			maxArmValue = maxArmValue[i]
+			maxArmValue = uHat[i]
 			maxArmI = i
 		else:
 			continue
+	print("Current max average arm is arm " + str(maxArmI) + " with value " + str(maxArmValue) + "\n")
 
+	print("Choosing based on coin toss: \n")
 	#Use epsilon to choose either a random arm or the currently best arm
 	if(randomValue > epsilon):
+		print("Chosen current best arm: \n")
 		#Choose the currently best arm
-		chosenArm = uHat[maxArmI]
+		chosenArm = u[maxArmI]
+		chosenArmI = maxArmI
 	else:
+		print("Chosen random arm: \n")
 		#choose a random arm
 		randomArmChoice = random.randint(0,armLength-1)
-		chosenArm = uHat[randomArmChoice]
+		chosenArm = u[randomArmChoice]
+		chosenArmI = randomArmChoice
+	print("Chosen arm is arm " + str(chosenArmI) + "\n")
 
-	#do something with the chosen arm
-	chosenArm
+	uHat[chosenArmI] = (uHat[chosenArmI]*(t) + chosenArm)/(t+1)
 
-
+	print("Updated average value for arm " + str(chosenArmI) + " to be " + str(uHat[chosenArmI]))
