c35f3af6ff7b8ec7bc72adc35910b3274fee5085
Graeme <gacrawley@gmail.com>
2018-02-25 15:09:30 -0500

    read a new paper to better understand. Now realise epsilon is set at the beginning, not randomized

--- multiArmedBandit.py
+++ multiArmedBandit.py
@@ -2,15 +2,21 @@ import random
 dist = [0,0,0,0,0]
 
 var = [0.1^2,0.01^2,0.05^2,0.03^2,0.08^2]
-p = [0,0,0,0,0,0,0,0,0,0]
 
 maxExp = 0
 
-comment: all turns
+epsilon = 0.1
+
+#all turns
 for t in range(100):
-	exp = [1,1,1,1,1]
+	uHat = [1,1,1,1,1]
+	p = [0,0,0,0,0,0,0,0,0,0]
+
 	for i in exp:
 		exp[i] = random.uniform(0,1)
+
+
+	#update the expected value every turn
 
 	comment: find the maximum expected value
 	for i in exp:
